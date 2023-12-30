39f8c8c964983cd956dd0d4db4683bdb3594f769
Ashraf Ali <ali4282271@gmail.com>
2023-12-29 21:08:03 -0800

    install tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2>My Front End Project</h2>
+     <h2 className='text-4xl text-red-500'>My Front End Project</h2>
     </div>
   );
 }
