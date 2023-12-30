fcae7ae964cb98ada76237c91742042d0781a9c7
Ashraf Ali <ali4282271@gmail.com>
2023-12-30 12:33:48 -0800

    create singUp page

---
+++ frontend/src/pages/signUp/index.js
@@ -0,0 +1,10 @@
+import { FormComponent } from "../../components/form"
+
+export const SingUp = () => {
+    return (
+        <>
+        <h1 className="text-2xl font-bold text-center">Hello User</h1>
+        <FormComponent signUp/>
+        </>
+    )
+}
