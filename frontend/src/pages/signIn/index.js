7bbcfc036ef9f3b29ba6bf3b8602e44e6eeb57f5
Ashraf Ali <ali4282271@gmail.com>
2023-12-30 12:34:01 -0800

    create signIn page

---
+++ frontend/src/pages/signIn/index.js
@@ -0,0 +1,9 @@
+import { FormComponent } from "../../components/form"
+export const SingIn = () => {
+    return (
+        <>
+        <h1 className="text-2xl font-bold text-center">Hello User</h1>
+        <FormComponent />
+        </>
+    )
+}
