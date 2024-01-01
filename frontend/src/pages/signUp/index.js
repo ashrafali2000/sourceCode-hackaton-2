fba1a97c8e638be14ab0e2b8f7b2de170d9572c9
oliverrupert <116807224+oliverrupert@users.noreply.github.com>
2024-01-01 10:02:10 +0500

    create signUpHandler

--- frontend/src/pages/signUp/index.js
+++ frontend/src/pages/signUp/index.js
@@ -1,6 +1,9 @@
 import { FormComponent } from "../../components/form"
 
 export const SingUp = () => {
+    const singUpHandler = () => {
+        
+    }
     return (
         <>
         <h1 className="text-2xl font-bold text-center">Hello User</h1>
