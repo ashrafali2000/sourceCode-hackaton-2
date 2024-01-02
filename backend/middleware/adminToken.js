42805d20c26d333ed777a74fdc727d4f59bd31e8
Ashraf Ali <ali4282271@gmail.com>
2024-01-02 19:40:34 +0500

    add adminMiddleware

---
+++ backend/middleware/adminToken.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { jwtSecretKey } = require("../constants");
+
+const verifyToken = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    console.log(authorization);
+    const token = authorization && authorization.split(" ")[1];
+    console.log(token);
+    // jwt.verify(token, jwtSecretKey , function (err, decoded) {
+    //     if (err) {
+    //         return res.status(401).send({ message: "User Nahi hai", err })
+    //     }
+    //     return next()
+    // })
+  } catch (err) {
+    return res.status(401).send({ err });
+  }
+};
+module.exports = verifyToken;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,45 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const { createUser, loginUser, getAllUser } = require("../controllers/user");
-const verifyToken = require("../middleware/verifyToken")
-const Joi = require("joi")
+const userToken = require("../middleware/userToken");
+const Joi = require("joi");
 
 // JOI Schema
 const joiUSerSchema = Joi.object({
-  firstName:Joi.string().required().min(5),
-  lastName:Joi.string().required().min(3),
-  email:Joi.string().required().email(),
-  password:Joi.string().min(6).required()
-})
-// get All User 
-router.get("/",verifyToken , async(req, res) => {
-  try{
+  firstName: Joi.string().required().min(5),
+  lastName: Joi.string().required().min(3),
+  email: Joi.string().required().email(),
+  password: Joi.string().min(6).required(),
+});
+// get All User
+router.get("/", userToken, async (req, res) => {
+  try {
     const users = await getAllUser();
-    return res.send({message:users})
-  }catch(err) {
-    return res.send({message:err})
+    return res.send({ message: users });
+  } catch (err) {
+    return res.send({ message: err });
   }
-})
+});
 // user CreateUser Controller
-router.post("/signup",  async (req, res) => {
-  try{
+router.post("/signup", async (req, res) => {
+  try {
     // await joiUSerSchema.validateAsync(req.body);
-    const {name, email, password} = req.body;
-    const response = await createUser({name, email, password});
-   return res.status(200).send({status:200, message: response});
-  }catch(err){
-   return res.status(400).send({status:400, message:err.message});
+    const { name, email, password } = req.body;
+    const response = await createUser({ name, email, password });
+    return res.status(200).send({ status: 200, message: response });
+  } catch (err) {
+    return res.status(400).send({ status: 400, message: err.message });
   }
 });
 
 // user LoginUser Controller
 router.post("/login", async (req, res) => {
-  try{
-    const { email, password} = req.body;
-    const response = await loginUser({email, password});
-   return res.status(200).send({status:200, user: response});
-  }catch(err){
-   return res.status(400).send({status:400, message:err.message});
+  try {
+    const { email, password } = req.body;
+    const response = await loginUser({ email, password });
+    return res.status(200).send({ status: 200, user: response });
+  } catch (err) {
+    return res.status(400).send({ status: 400, message: err.message });
   }
 });
 module.exports = router;
