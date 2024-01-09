1608cc48794b3f2ae20a85df136d92fd8833adca
MuhammadNabeelNisarWorkspace <ali4282271@gmail.com>
2024-01-09 11:10:07 +0500

    update pasward keyword

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
-const password = process.env.PASSWORD
- mongoose.connect(`mongodb+srv://Ashrafali:${password}@ashratech.fbnrqrf.mongodb.net/source-code-hackaton-2?retryWrites=true&w=majority`)
-module.exports = mongoose
+require("dotenv").config();
+const password = process.env.DB_PASSWORD;
+mongoose.connect(
+  `mongodb+srv://Ashrafali:${password}@ashratech.fbnrqrf.mongodb.net/source-code-hackaton-2?retryWrites=true&w=majority`
+);
+module.exports = mongoose;
